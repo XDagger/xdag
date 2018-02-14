64b9f92c6d353f984409b3bcc2b285cc91f2d147
Jonano <jonano614@mail.ru>
2018-02-14 03:34:52 +0300

    Code-style and comments for address.c and block.c

--- client/address.c
+++ client/address.c
@@ -1,4 +1,4 @@
-/* адреса, T13.692-T13.720 $DVS:time$ */
+/* addresses, T13.692-T13.720 $DVS:time$ */
 
 #include <stdint.h>
 #include <string.h>
@@ -7,43 +7,69 @@
 static const uint8_t bits2mime[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 static uint8_t mime2bits[256];
 
-/* инициализировать модуль адресов */
-int cheatcoin_address_init(void) {
-	int i;
-	memset(mime2bits, 0xFF, 256);
-	for (i = 0; i < 64; ++i) mime2bits[bits2mime[i]] = i;
-	return 0;
+//intialization of addresses module
+int cheatcoin_address_init(void)
+{
+    int i;
+    memset(mime2bits, 0xFF, 256);
+    for(i = 0; i < 64; ++i)
+    {
+        mime2bits[bits2mime[i]] = i;
+    }
+    return 0;
 }
 
-/* преобразовать адрес в хеш */
-int cheatcoin_address2hash(const char *address, cheatcoin_hash_t hash) {
-	uint8_t *fld = (uint8_t *)hash;
-	int i, c, d, e, n;
-	for (e = n = i = 0; i < 32; ++i) {
-		do {
-			if (!(c = (uint8_t)*address++)) return -1;
-			d = mime2bits[c];
-		} while (d & 0xC0);
-		e <<= 6, e |= d, n += 6;
-		if (n >= 8) {
-			*fld++ = e >> (n -= 8);
-		}
-	}
-	for (i = 0; i < 8; ++i) *fld++ = 0;
-	return 0;
+//converts address to hash
+int cheatcoin_address2hash(const char *address, cheatcoin_hash_t hash)
+{
+    uint8_t *fld = (uint8_t *)hash;
+    int i, c, d, e, n;
+    for(e = n = i = 0; i < 32; ++i)
+    {
+        do
+        {
+            if(!(c = (uint8_t)*address++))
+            {
+                return -1;
+            }
+            d = mime2bits[c];
+        }
+        while(d & 0xC0);
+        e <<= 6;
+        e |= d;
+        n += 6;
+        if(n >= 8)
+        {
+            n -= 8;
+            *fld++ = e >> n;
+        }
+    }
+    for(i = 0; i < 8; ++i)
+    {
+        *fld++ = 0;
+    }
+    return 0;
 }
 
-/* преобразовать хеш в адрес */
-const char *cheatcoin_hash2address(const cheatcoin_hash_t hash) {
-	static char bufs[4][33];
-	static int k = 0;
-	char *buf = &bufs[k++ & 3][0], *ptr = buf;
-	int i, c, d;
-	const uint8_t *fld = (const uint8_t *)hash;
-	for (i = c = d = 0; i < 32; ++i) {
-		if (d < 6) d += 8, c <<= 8, c |= *fld++;
-		*ptr++ = bits2mime[c >> (d -= 6) & 0x3F];
-	}
-	*ptr = 0;
-	return buf;
+//converts hash to address
+const char *cheatcoin_hash2address(const cheatcoin_hash_t hash)
+{
+    static char bufs[4][33];
+    static int k = 0;
+    char *buf = &bufs[k++ & 3][0], *ptr = buf;
+    int i, c, d;
+    const uint8_t *fld = (const uint8_t *)hash;
+    for(i = c = d = 0; i < 32; ++i)
+    {
+        if(d < 6)
+        {
+            d += 8;
+            c <<= 8;
+            c |= *fld++;
+        }
+        d -= 6;
+        *ptr++ = bits2mime[c >> d & 0x3F];
+    }
+    *ptr = 0;
+    return buf;
 }
