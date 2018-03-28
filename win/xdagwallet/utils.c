a526ad799918dd415c509508302beda9fd71949d
sgaragagghu <sgaragagghu@gmail.com>
2018-03-29 01:52:17 +0200

    Update utils.c

--- win/xdagwallet/utils.c
+++ win/xdagwallet/utils.c
@@ -2,6 +2,10 @@
 
 
 int conversion_toInt(long double a, int* b) {
+	if (INT_MAX > LDBL_MAX) {
+		fprintf(stderr, "Can't check the conversion, it isn't safe");
+		exit(EXIT_FAILURE);
+	}
 	if (a > (long double)INT_MAX || a < (long double)-INT_MAX)		
 		return -1;
 	*b = (int)a;
@@ -9,6 +13,10 @@ int conversion_toInt(long double a, int* b) {
 }
 
 int conversion_toLong(long double a, long* b) {
+	if (LONG_MAX > LDBL_MAX) {
+		fprintf(stderr, "Can't check the conversion, it isn't safe");
+		exit(EXIT_FAILURE);
+	}
 	if (a > (long double)LONG_MAX || a < (long double)-LONG_MAX)
 		return -1;
 	*b = (long)a;
