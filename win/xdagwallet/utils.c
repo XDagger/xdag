33e2b327c47ae61e81a2c047e74d593cfc22a19b
sgaragagghu <sgaragagghu@gmail.com>
2018-03-29 01:39:43 +0200

    Update utils.c

--- win/xdagwallet/utils.c
+++ win/xdagwallet/utils.c
@@ -2,17 +2,15 @@
 
 
 int conversion_toInt(long double a, int* b) {
-	if (a > (long double)INT_MAX) {
+	if (a > (long double)INT_MAX || a < (long double)-INT_MAX)		
 		return -1;
-	}
 	*b = (int)a;
 	return 0;
 }
 
 int conversion_toLong(long double a, long* b) {
-	if (a > (long double)LONG_MAX) {
+	if (a > (long double)LONG_MAX || a < (long double)-LONG_MAX)
 		return -1;
-	}
 	*b = (long)a;
 	return 0;
 }
