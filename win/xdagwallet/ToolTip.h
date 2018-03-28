2292fcdb0520cf623ccc2559e11c8128f7527802
sgaragagghu <sgaragagghu@gmail.com>
2018-03-28 22:46:47 +0200

    Update ToolTip.h

--- win/xdagwallet/ToolTip.h
+++ win/xdagwallet/ToolTip.h
@@ -10,21 +10,20 @@ class CToolTip : public CFrameWnd
 public:
 
     static CToolTip* Show(
-               CPoint pt,			// point where there will be the tooltip 
+               CPoint pt,				// point where there will be the tooltip 
 			   LPRECT lpRect,		// rect of the parent
-               int nWidth,			// mean character width
+               int nWidth,				// mean character width
 			   int nHeight,			// mean character height
                CString strMessage,
                UINT nSecs
             );
-
-    // 
+	    
 	static CWnd	* pParentWindow;
 	
 
 protected:
 
-  	CToolTip(CString strMessage);
+    CToolTip(CString strMessage);
     ~CToolTip();
     BOOL Create(CRect rect);
     void MakeVisisble(UINT nSecs);	
@@ -34,12 +33,12 @@ protected:
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	afx_msg void OnPaint();
 	afx_msg void OnActivateApp(BOOL bActive, DWORD hTask);
-    virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+    	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 
 // Attributes
 	CRect   m_rectText; 
-    CRect   m_rectWindow;   // Rectangle of the tooltip
-    CString m_strMessage;
+   	CRect   m_rectWindow;   	 // Rectangle of the tooltip
+    	CString m_strMessage;
 	CRgn    m_rgnRoundRect;   // The region of the round rectangle  
-    CWnd    m_wndInvisibleParent; // invisible taskbare window to contain the tooltip
+    	CWnd    m_wndInvisibleParent; // invisible taskbare window to contain the tooltip
 };
