720bcc5f6988183c31bd2913eaa9c5ab70b60c43
sgaragagghu <sgaragagghu@gmail.com>
2018-04-06 10:19:31 +0200

    Update ToolTip.h

--- win/xdagwallet/ToolTip.h
+++ win/xdagwallet/ToolTip.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+This software will show a squared tooltip.
+OVERFLOWS CHECKING WILL NOT BE STRESSED
+since the applications are not critical.
+
+Copyright (C) 2018  Marco Scarlino <marco.scarlino@students-live.uniroma2.it>.
+
+ToolTip (consisting of ToolTip.cpp and ToolTip.h) is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+***************************************************************************/
+
 #pragma once
 #include "stdafx.h"
 
