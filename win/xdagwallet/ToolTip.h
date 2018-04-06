7c1133b02e536d6c8c87a9ed60dca664dbb4e870
sgaragagghu <sgaragagghu@gmail.com>
2018-04-06 12:13:37 +0200

    Update ToolTip.h

--- win/xdagwallet/ToolTip.h
+++ win/xdagwallet/ToolTip.h
@@ -6,14 +6,17 @@ since the applications are not critical.
 Copyright (C) 2018  Marco Scarlino <marco.scarlino@students-live.uniroma2.it>.
 
 ToolTip (consisting of ToolTip.cpp and ToolTip.h) is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
+modify it under the terms of the GNU Lesser General Public License
 as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************************/
 
 #pragma once
