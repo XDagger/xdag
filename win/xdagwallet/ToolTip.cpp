f0c60e88577e72d10adbbf7b1b55fefe8f97716d
sgaragagghu <sgaragagghu@gmail.com>
2018-04-06 10:19:09 +0200

    Update ToolTip.cpp

--- win/xdagwallet/ToolTip.cpp
+++ win/xdagwallet/ToolTip.cpp
@@ -5,7 +5,7 @@ since the applications are not critical.
 
 Copyright (C) 2018  Marco Scarlino <marco.scarlino@students-live.uniroma2.it>.
 
-Tooltip is free software; you can redistribute it and/or
+ToolTip (consisting of ToolTip.cpp and ToolTip.h) is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version.
