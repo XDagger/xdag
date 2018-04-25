cfbbf898f41f33ba0206249d60f05af6ab1bf2d4
sgaragagghu <sgaragagghu@gmail.com>
2018-04-26 00:24:31 +0200

    Update pool.c

--- client/pool.c
+++ client/pool.c
@@ -465,12 +465,13 @@ static void calculate_nopaid_shares(struct connection_pool_data *conn_data, stru
 		// of the 256 bit number hash[3] || hash[2] || hash[1] || hash[0].
 
 		diff = ldexp(diff, -64);
-		diff += ((uint64_t*)hash)[3];
-					// Since diff is unsigned, diff < 1 implies diff=0 and log(diff) function is not defined for diff=0.
-		if(diff < 1) diff = 1; 	// The "difficultiest" hash sent by miner implies diff=1 (since this is the case of hash[3] is 0) and
-					// log(1)=0, thus maximum diff value, at this point, is 46. 
-		diff = 46 - log(diff);	// 
-					//
+		diff += ((uint64_t*)hash)[3]; // Since diff is unsigned, diff < 1 implies diff=0 and log(diff) function is not defined for diff=0.
+					      // The "most difficult" hash sent by miner implies diff=1 (since this is the case of hash[3] is 0) and	
+		if(diff < 1) diff = 1;	      // log(1)=0, thus maximum diff value, at this point, is 46. The "easiest" hash, instead, would lay on
+					      // the same result that is diff 46 (that's the case hash[3]=hash[2]=0xFFFFFFFFFFFFFFFF, hash[3]+1=0 
+		diff = 46 - log(diff);	      // thus it's the same as the most difficult hash), it is probably a bug. Let's consider an "almost easiest" hash
+					      // like hash[3]=FFFFFFFFFFFFFFFF and hash[2]<=FFFFFFFFFFFFFBFF, in this case we have 46-log(FFFFFFFFFFFFFFFF)=46-19=27.
+					      // At this point diff seems to have a range [46;27], where higher value is higher difficulty.
 		if(conn_data->task_time < task_time) {
 			conn_data->task_time = task_time;
 
