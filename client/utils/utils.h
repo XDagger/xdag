dce4a8187ab2cb00f667a135525bffd8aaebb90e
Rui Xie <xrdavies@gmail.com>
2018-04-30 22:15:10 +0800

    add feature to detect dead lock

--- client/utils/utils.h
+++ client/utils/utils.h
@@ -26,4 +26,38 @@ extern void xdag_close_file(FILE *f);
 extern int xdag_file_exists(const char *path);
 extern int xdag_mkdir(const char *path);
 
+/* dead lock detector */
+
+#define DEAD_LOCK_DETECT 1
+extern void start_check_deadlock_thread(void);
+extern void apply_lock_before(uint64_t tid, pthread_mutex_t* mutex_ptr, const char* name);
+extern void apply_lock_after(uint64_t tid, pthread_mutex_t* mutex_ptr);
+extern void apply_unlock(uint64_t tid, pthread_mutex_t* mutex_ptr);
+
+extern void test_deadlock(void);
+
+#if DEAD_LOCK_DETECT
+#define XDAG_MUTEX_TRYLOCK(x) pthread_mutex_trylock(&x)
+
+#define XDAG_MUTEX_LOCK(x) \
+do {\
+	apply_lock_before(pthread_self(), &x, #x);\
+	pthread_mutex_trylock(&x);\
+	apply_lock_after(pthread_self(), &x);\
+} while(0)
+
+#define XDAG_MUTEX_UNLOCK(x) \
+do {\
+	pthread_mutex_unlock(&x);\
+	apply_unlock(pthread_self(), &x);\
+} while(0)
+	
+#else
+#define XDAG_MUTEX_TRYLOCK(x) pthread_mutex_trylock(&x)
+#define XDAG_MUTEX_LOCK(x) pthread_mutex_lock(&x)
+#define XDAG_MUTEX_UNLOCK(x) pthread_mutex_unlock(&x)
+#endif
+
+
+
 #endif /* utils_h */
