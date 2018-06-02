60884018dd7a9e8f0e273565d6b2270aecb17d66
sgaragagghu <sgaragagghu@gmail.com>
2018-06-02 14:35:53 +0200

    Update pool.c

--- client/pool.c
+++ client/pool.c
@@ -532,14 +532,14 @@ static void calculate_nopaid_shares(struct connection_pool_data *conn_data, stru
 		// %%%%%% 		           diff 			     %%%%%%
 		// At this point, diff, seems to be a condensate approximated representation 
 		// of the 256 bit number hash[3] || hash[2] || hash[1] || hash[0].
-									//**** THE COMMENT BELOW IS WRONG**//
-		diff = ldexp(diff, -64);	// Since diff is unsigned, diff < 1 implies diff=0 and log(diff) function is not defined for diff=0, it is needed to eliminate
+									
+		diff = ldexp(diff, -64);	// Since diff is unsigned, diff < 1 implies diff=0 and ln(diff) function is not defined for diff=0, it is needed to eliminate
 						// the diff=0 case (if(diff < 1) diff = 1). The "most difficult" hash sent by miner implies diff=1 (since this is the case of hash[3] and hash[2] is 0) 
-		diff += ((uint64_t*)hash)[3];   // and log(1)=0, thus maximum diff value, at this point, is 46. The "easiest" hash, instead, would lay on
+		diff += ((uint64_t*)hash)[3];   // and ln(1)=0, thus maximum diff value, at this point, is 46. The "easiest" hash, instead, would lay on
 					        // the same result that is diff 46 (that's the case hash[3]=hash[2]=0xFFFFFFFFFFFFFFFF, hash[3]+1=0 
 		if(diff < 1) diff = 1;          // thus it's the same as the most difficult hash), it is probably a bug. Let's consider an "almost easiest" hash
-					        // like hash[3]=FFFFFFFFFFFFFFFF and hash[2]<=FFFFFFFFFFFFFBFF, in this case we have 46-log(FFFFFFFFFFFFFFFF)=46-19=27.
-		diff = 46 - log(diff);		// At this point diff seems to have a range [46;27], where higher value is higher difficulty.
+		//log is ln		        // like hash[3]=FFFFFFFFFFFFFFFF and hash[2]<=FFFFFFFFFFFFFBFF, in this case we have 46-ln(FFFFFFFFFFFFFFFF)=46-44=2.
+		diff = 46 - log(diff);		// At this point diff seems to have a range [46;2], where higher value is higher difficulty.
 		
 		// Adding share for connection
 		if(conn_data->task_time < task_time) { // conn_data->task_time will keep old value until pool doesn't accept the share of the task.
