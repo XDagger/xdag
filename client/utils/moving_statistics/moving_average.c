37da66558ff580725bd8a6da083b39d568c3242d
sgaragagghu <sgaragagghu@gmail.com>
2018-06-05 18:26:28 +0200

    Update moving_average.c

--- client/utils/moving_statistics/moving_average.c
+++ client/utils/moving_statistics/moving_average.c
@@ -5,7 +5,7 @@ Thanks to B.P.Welford (1962) [Donald Knuth's Art of Computer Programming, Vol 2,
 Copyright (C) 2018  Marco Scarlino <marco.scarlino@students-live.uniroma2.it>.
 moving_average (consisting of moving_average.c and moving_average.h) is free software; 
 you can redistribute it and/or modify it.
-If you modify this file, you have to release the source code of this file included all of the modifications apported.
+If you modify this file, you have to release the source code of this file included all of the modifications apported to it.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
