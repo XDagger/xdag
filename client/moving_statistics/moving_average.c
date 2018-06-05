0249917fca954f76b93edf0ba346c1e3b30a57f1
sgaragagghu <sgaragagghu@gmail.com>
2018-06-05 17:29:20 +0200

    Update moving_average.c

--- client/moving_statistics/moving_average.c
+++ client/moving_statistics/moving_average.c
@@ -1,18 +1,14 @@
 /***************************************************************************
-This software will calulate the moving average.
+This software will calculate the moving average.
 Thanks to B.P.Welford (1962) [Donald Knuth's Art of Computer Programming, Vol 2, page 232, 3rd edition]
 
 Copyright (C) 2018  Marco Scarlino <marco.scarlino@students-live.uniroma2.it>.
-moving_average (consisting of moving_average.c and moving_average.h) is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or (at your option) any later version.
+moving_average (consisting of moving_average.c and moving_average.h) is free software; 
+you can redistribute it and/or modify it.
+If you modify this file, you have to release the source code of the modified file.
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ***************************************************************************/
 
 #include "moving_average.h"
