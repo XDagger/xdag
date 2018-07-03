f4b50dc055a483c4117bc38606b8bc6ed50e75fc
liujun <liujun@followme.com>
2018-07-03 15:53:36 +0800

    add rpc white host

--- rpc/httprpc.h
+++
@@ -1,9 +0,0 @@
-/* httprpc, T13.670-T13.788 $DVS:time$ */
-
-#ifndef XDAG_HTTP_RPC_H
-#define XDAG_HTTP_RPC_H
-
-extern int http_rpc_start(const char *username ,const char *passwd, int port);
-extern int http_rpc_command(void *out, char *type, const char *address);
-
-#endif
