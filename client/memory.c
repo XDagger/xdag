4bca94a60b3e72bb63d7ae359524b203b28f7faa
marco <sgaragagghu@gmail.com>
2018-08-22 17:10:35 +0200

    When tmpfile is full a new one will be created

--- client/memory.c
+++ client/memory.c
@@ -1,9 +1,11 @@
-/* memory managment, T13.816-T14.330 $DVS:time$ */
+/* memory managment, T13.816-T14.423 $DVS:time$ */
 
 #include <stdlib.h>
 #include <stdint.h>
 #include "memory.h"
 #include "utils/log.h"
+
+int g_use_tmpfile = 0;
 
 #if defined(_WIN32) || defined(_WIN64)
 
@@ -49,12 +51,18 @@ int xdag_free_all(void)
 #define TMPFILE_TEMPLATE 	"xdag-tmp-XXXXXX"
 #define TMPFILE_TEMPLATE_LEN	15
 
-static int g_fd = -1;
+struct temp_file {
+	int fd;
+	void *mem;
+	char tmpfile[PATH_MAX];
+	struct temp_file *prev;
+};
+
 static size_t g_pos = 0, g_fsize = 0, g_size = 0;
-static void *g_mem;
 static pthread_mutex_t g_mem_mutex = PTHREAD_MUTEX_INITIALIZER;
 static char g_tmpfile_path[PATH_MAX] = "";
-static char g_tmpfile[PATH_MAX];
+static struct temp_file *g_last_tmpfile = NULL;
+void delete_actual_tmpfile(void);
 
 void xdag_mem_tempfile_path(const char *tempfile_path)
 {
@@ -63,37 +71,60 @@ void xdag_mem_tempfile_path(const char *tempfile_path)
 
 int xdag_mem_init(size_t size)
 {
+	struct temp_file *tfile_node;
 	if (!size) {
 		return 0;
 	}
 	if(strcmp(g_tmpfile_path,"RAM") == 0) {
-		/* This will leave g_fd as -1, and malloc will then be called in xdag_malloc instead of pointer into mmap */
+		/* This will leave g_use_tmpfile as 0, and malloc will then be called in xdag_malloc instead of pointer into mmap */
 		return 0;
 	}
 
-	size--;
-	size |= MEM_PORTION - 1;
-	size++;
+	tfile_node = calloc(sizeof(struct temp_file),1);
+	if(tfile_node == NULL){
+		xdag_warn("Error: creation of tmp file failed because calloc failed [function: xdag_mem_init]");
+		return -1;
+	}
 
-	size_t wrote = snprintf(g_tmpfile, PATH_MAX,"%s%s", g_tmpfile_path, TMPFILE_TEMPLATE);
+	if(g_last_tmpfile == NULL) {
+		size--;
+		size |= MEM_PORTION - 1;
+		size++;
+	}
+
+	size_t wrote = snprintf(tfile_node->tmpfile, PATH_MAX,"%s%s", g_tmpfile_path, TMPFILE_TEMPLATE);
 	if (wrote >= PATH_MAX){
 		xdag_fatal("Error: Temporary file path exceed the max length that is %d characters", PATH_MAX);
+		free(tfile_node);
 		return -1;
 	}
-	g_fd = mkstemp(g_tmpfile);
-	if (g_fd < 0) {
-		xdag_fatal("Unable to create temporary file %s errno:%d", g_tmpfile, errno);
+	tfile_node->fd = mkstemp(tfile_node->tmpfile);
+	if (tfile_node->fd < 0) {
+		xdag_fatal("Unable to create temporary file %s errno:%d", tfile_node->tmpfile, errno);
+		free(tfile_node);
 		return -1;
 	}
-        printf("Temporary file created: %s\n", g_tmpfile);
+        xdag_info("Temporary file created: %s\n", tfile_node->tmpfile);
 
-	g_mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, g_fd, 0);
-	if (g_mem == MAP_FAILED) {
-		close(g_fd); return -1;
+	tfile_node->mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, tfile_node->fd, 0);
+	if (tfile_node->mem == MAP_FAILED) {
+		close(tfile_node->fd);
+		free(tfile_node);
+		return -1;
 	}
 
-	g_size = size;
-	
+	if(g_last_tmpfile == NULL) {
+		g_size = size;
+	} else {
+		g_pos = 0;
+		g_fsize = 0;
+	}
+
+	tfile_node->prev = g_last_tmpfile;
+	g_last_tmpfile = tfile_node;
+
+	g_use_tmpfile = 1;
+
 	return 0;
 }
 
@@ -101,7 +132,12 @@ void *xdag_malloc(size_t size)
 {
 	uint8_t *res;
 
-	if (g_fd < 0) {
+	if (!g_use_tmpfile) {
+		return malloc(size);
+	}
+
+	if(g_last_tmpfile == NULL){
+		xdag_warn("Unexpected state [function xdag_malloc]");
 		return malloc(size);
 	}
 
@@ -117,14 +153,17 @@ void *xdag_malloc(size_t size)
 
 	while (g_pos + size > g_fsize) {
 		if (g_fsize + MEM_PORTION > g_size) {
-			pthread_mutex_unlock(&g_mem_mutex);
-			return 0;
+			int res = xdag_mem_init(g_size);
+			if(res){
+				pthread_mutex_unlock(&g_mem_mutex);
+				return 0;
+			}
 		}
 		g_fsize += MEM_PORTION;
-		ftruncate(g_fd, g_fsize);
+		ftruncate(g_last_tmpfile->fd, g_fsize);
 	}
 
-	res = (uint8_t*)g_mem + g_pos;
+	res = (uint8_t*)(g_last_tmpfile->mem) + g_pos;
 	g_pos += size;
 	
 	pthread_mutex_unlock(&g_mem_mutex);
@@ -138,20 +177,37 @@ void xdag_free(void *mem)
 
 void xdag_mem_finish(void)
 {
-	if (g_fd < 0) return;
+	if (!g_use_tmpfile) {
+		return;
+	}
 	
 	pthread_mutex_lock(&g_mem_mutex);
-	
-	munmap(g_mem, g_size);
-	ftruncate(g_fd, 0);
-	close(g_fd);
-	remove(g_tmpfile);
+	for (struct temp_file *next = NULL;
+			g_last_tmpfile != NULL;
+			next = g_last_tmpfile, g_last_tmpfile = g_last_tmpfile->prev, free(next)) {
+		delete_actual_tmpfile();
+	}
 }
 
 int xdag_free_all(void)
 {
+	if (!g_use_tmpfile) {
+		return -1;
+	}
+	for(struct temp_file *next = NULL;
+			g_last_tmpfile != NULL && g_last_tmpfile->prev != NULL;
+			next = g_last_tmpfile, g_last_tmpfile = g_last_tmpfile->prev, free(next)){
+		delete_actual_tmpfile();
+	}
 	g_pos = 0;
 	return 0;
+}
+
+void delete_actual_tmpfile(){
+	munmap(g_last_tmpfile->mem, g_size);
+	ftruncate(g_last_tmpfile->fd, 0);
+	close(g_last_tmpfile->fd);
+	remove(g_last_tmpfile->tmpfile);
 }
 
 #endif
