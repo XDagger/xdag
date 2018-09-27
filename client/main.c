2642af989fabdf93e6c643e59f01d9471040077d
Frozen <xrdavies@gmail.com>
2018-09-27 12:07:38 +0800

    remove useless lines

--- client/main.c
+++ client/main.c
@@ -1,12 +1,8 @@
 /* xdag main, T13.654-T13.895 $DVS:time$ */
 
 #include "init.h"
-
-#include "utils/utils.h"
 int main(int argc, char **argv)
 {
 	xdag_init(argc, argv, 0);
-//	char a[] = "123456789012345678901234567890abcde";
-//	printf("valide remark : %ld\n", validate_ascii_safe(a, 5));
 	return 0;
 }
