6050122a88a5515f5bca7114bfb3c2af5fe99596
Evgeniy <jonano614@mail.ru>
2018-11-01 00:56:26 +0300

    atomic_exchange function for windows

--- win/stdatomic.c
+++ win/stdatomic.c
@@ -3,6 +3,11 @@
 
 #include "stdatomic.h"
 
+uint_least64_t atomic_exchange(atomic_uint_least64_t *object, uint_least64_t desired)
+{
+	return _InterlockedExchange64((volatile long long*)object, desired);
+}
+
 int atomic_compare_exchange_strong(atomic_int *ptr, int *expected, int desired)
 {
 	int comparand = *expected;
