06e62818e3aab06546079b922a4cd5cc956d23cc
Frozen <xrdavies@gmail.com>
2018-11-06 21:02:58 +0800

    change to use compressed json

--- client/json-rpc/rpc_service.c
+++ client/json-rpc/rpc_service.c
@@ -121,7 +121,7 @@ static void* rpc_handle_thread(void *arg)
 	}
 	
 	cJSON * result = xdag_rpc_handle_request(body);
-	char *response = cJSON_Print(result);
+	char *response = cJSON_PrintUnformatted(result);
 	send_response(conn, response);
 	free(response);
 	
