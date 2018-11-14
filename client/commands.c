8b2933d9b6fb8d8694e0a5cc20179ce5e941e0e2
Evgeniy <jonano614@mail.ru>
2018-11-14 22:12:23 +0300

    Update commands.c

--- client/commands.c
+++ client/commands.c
@@ -545,7 +545,8 @@ void processStatsCommand(FILE *out)
 			(long long)g_xdag_extstats.nnoref,
 			g_xdag_extstats.nwaitsync,
 			xdag_diff_args(g_xdag_stats.difficulty), xdag_diff_args(g_xdag_stats.max_difficulty),
-			g_coinname, amount2xdags(xdag_get_supply(g_xdag_stats.nmain)), amount2xdags(xdag_get_supply(g_xdag_stats.total_nmain)),
+			g_coinname, 
+			amount2xdags(xdag_get_supply(g_xdag_stats.nmain)), amount2xdags(xdag_get_supply(g_xdag_stats.total_nmain)),
 			xdag_hashrate(g_xdag_extstats.hashrate_ours), xdag_hashrate(g_xdag_extstats.hashrate_total)
 		);
 	}
