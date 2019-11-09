5fc4557443518b804987bd62aa7b19e94b482f3d
reymondtu <reymondtu@gmx.com>
2019-11-10 01:44:22 +0800

    recovery testnet default value

--- client/global.c
+++ client/global.c
@@ -1,7 +1,7 @@
 #include "global.h"
 
 int g_xdag_state = XDAG_STATE_INIT;
-int g_xdag_testnet = 1;
+int g_xdag_testnet = 0;
 int g_xdag_run = 0;
 enum xdag_field_type g_block_header_type = XDAG_FIELD_HEAD_TEST;
 struct xdag_stats g_xdag_stats;
