e2221fdd197ed0f19abb7ca80e787897f3e11ae5
reymondtu <reymondtu@gmx.com>
2020-04-27 00:14:44 +0800

    update xdag version to 0.4.0

--- client/version.h
+++ client/version.h
@@ -7,8 +7,8 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               0
-#define VERSION_MINOR               3
-#define VERSION_REVISION            1
+#define VERSION_MINOR               4
+#define VERSION_REVISION            0
 
 #define XDAG_VERSION STRINGIZE(VERSION_MAJOR) "." STRINGIZE(VERSION_MINOR) "." STRINGIZE(VERSION_REVISION)
 
