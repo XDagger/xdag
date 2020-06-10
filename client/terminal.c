43219123313f7cb032516758b3f78b7f4818db04
holt666 <holt666@gmx.com>
2020-06-10 13:16:08 +0800

    fix unix domain os diff

--- client/terminal.c
+++ client/terminal.c
@@ -9,8 +9,6 @@
 
 uv_loop_t *loop;
 uv_pipe_t server_pipe;
-uv_pipe_t client_pipe;
-uv_connect_t conn;
 uv_tty_t tty_stdout;
 uv_work_t work;
 typedef struct {
@@ -22,6 +20,12 @@ typedef struct {
     uint8_t type;
     char *cmd;
 } xdag_cmd_t;
+
+typedef struct {
+    size_t recved_length;
+    size_t recv_size;
+    char *recv_buf;
+} xdag_session_t;
 
 static void command_work(uv_work_t* work);
 static void command_complete(uv_work_t* work, int status);
@@ -35,6 +39,8 @@ static void free_write_req(uv_write_t *req) {
 }
 
 static void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+    xdag_session_t *xd_session = (xdag_session_t*)handle->data;
+
     buf->base = (char*)malloc(suggested_size);
     buf->len = suggested_size;
 }
@@ -53,13 +59,15 @@ static void exec_xdag_command(uv_handle_t* handle, char *cmd) {
         uv_close((uv_handle_t*) handle, NULL);
     }
     fseek(tmp_fp, 0, SEEK_END);
-    int len = ftell(tmp_fp);
+    size_t len = ftell(tmp_fp);
     fseek(tmp_fp, 0, SEEK_SET);
 
     if(len > 0) {
         write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
-        req->buf = uv_buf_init((char*) malloc(len), len);
-        fread(req->buf.base, len, 1, tmp_fp);
+        len += sizeof(size_t);
+        req->buf = uv_buf_init((char*) malloc(len) , len);
+        memcpy(req->buf.base, &len, sizeof(size_t));
+        fread(req->buf.base + sizeof(size_t), len - sizeof(size_t), 1, tmp_fp);
         uv_write((uv_write_t*) req, (uv_stream_t*)handle, &req->buf, 1, on_server_write);
     }
     fclose(tmp_fp);
@@ -157,31 +165,30 @@ static void command_work(uv_work_t* work) {
     }
 }
 
-static void on_stdout_write_without_work(uv_write_t* req, int status) {
-    if(status){
-        fprintf(stderr, "tty write error %s\n", uv_strerror(status));
-        exit(0);
-    }
-    free_write_req(req);
-}
-
 static void on_client_read_pipe(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf){
-    if(nread > 0) {
-        write_req_t *wri = (write_req_t *)malloc(sizeof(write_req_t));
-        wri->buf = uv_buf_init(buf->base, nread);
-        // libuv cannot read more than 8192(macos) or 4096(linux) bytes at one time
-        if(nread == 8192 || nread == 4096) {
-            uv_write((uv_write_t*)wri,(uv_stream_t*)&tty_stdout, &wri->buf,1, on_stdout_write_without_work);
-        } else {
-            uv_write((uv_write_t*)wri,(uv_stream_t*)&tty_stdout, &wri->buf,1, on_stdout_write);
-        }
-    }
-
+    xdag_session_t  *xd_session = (xdag_session_t*)stream->data;
     if (nread < 0) {
         if (nread != UV_EOF) {
             fprintf(stderr, "pipe read error %s\n", uv_err_name(nread));
         }
         uv_close((uv_handle_t*) stream, NULL);
+    } else if(nread > 0) {
+        if(xd_session->recv_size == 0) {
+            memcpy(&(xd_session->recv_size), buf->base, sizeof(size_t));
+            xd_session->recv_buf = malloc(xd_session->recv_size);
+            memset(xd_session->recv_buf, 0, xd_session->recv_size);
+        }
+
+        memcpy(xd_session->recv_buf + xd_session->recved_length, buf->base, nread);
+        xd_session->recved_length += nread;
+        if(xd_session->recved_length >= xd_session->recv_size) {
+            write_req_t *wri = (write_req_t *)malloc(sizeof(write_req_t));
+            wri->buf = uv_buf_init((char*)malloc(xd_session->recv_size), xd_session->recv_size);
+            memcpy(wri->buf.base, xd_session->recv_buf + sizeof(size_t), xd_session->recv_size - sizeof(size_t));
+            uv_write((uv_write_t*)wri,(uv_stream_t*)&tty_stdout, &wri->buf,1, on_stdout_write);
+            free(xd_session->recv_buf);
+            memset(xd_session, 0, sizeof(xdag_session_t));
+        }
     }
 }
 
@@ -198,10 +205,9 @@ static void command_complete(uv_work_t* work, int status) {
         memset(wri->buf.base, 0, strlen(cmd));
         uv_write((uv_write_t*)wri, (uv_stream_t*)&tty_stdout, &wri->buf, 1, on_stdout_write);
     } else if(xdag_cmd->type == 1) {
-        uv_write((uv_write_t*)wri, (uv_stream_t*)&client_pipe, &wri->buf, 1, on_client_write_pipe);
-        uv_read_start((uv_stream_t*)&client_pipe, alloc_buffer, on_client_read_pipe);
+        uv_write((uv_write_t*)wri, (uv_stream_t*)&server_pipe, &wri->buf, 1, on_client_write_pipe);
+        uv_read_start((uv_stream_t*)&server_pipe, alloc_buffer, on_client_read_pipe);
     }
-
 }
 
 static void on_stdout_write(uv_write_t* req, int status) {
@@ -225,19 +231,24 @@ static void on_client_connect(uv_connect_t* req, int status) {
     if(status < 0){
         fprintf(stderr, "pipe new conect error...\n");
     }
-    uv_read_start((uv_stream_t*)&client_pipe, alloc_buffer, on_client_read_pipe);
+    xdag_session_t *xd_session = malloc(sizeof(xdag_session_t));
+    memset(xd_session, 0, sizeof(xdag_session_t));
+    uv_handle_set_data((uv_handle_t*)&server_pipe, xd_session);
+    uv_read_start((uv_stream_t*)&server_pipe, alloc_buffer, on_client_read_pipe);
 }
 
 void terminal_client(void *arg) {
     xdag_init_commands();
     loop = uv_default_loop();
-    uv_pipe_init(loop, &client_pipe, 0);
+    uv_pipe_init(loop, &server_pipe, 0);
     uv_tty_init(loop, &tty_stdout,1,0);
+
     xdag_cmd_t *xdag_cmd = malloc(sizeof(xdag_cmd_t));
     memset(xdag_cmd, 0, sizeof(xdag_cmd_t));
     xdag_cmd->type = 1;
     work.data = xdag_cmd;
     uv_queue_work(loop, &work, command_work, command_complete);
-    uv_pipe_connect((uv_connect_t*)&conn, &client_pipe, UNIX_SOCK, on_client_connect);
+    uv_connect_t conn;
+    uv_pipe_connect((uv_connect_t*)&conn, &server_pipe, UNIX_SOCK, on_client_connect);
     uv_run(loop, UV_RUN_DEFAULT);
 }
