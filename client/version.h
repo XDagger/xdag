558884a45a3017b87b9f8dc07d53281dad151a97
holt666 <holt666@gmx.com>
2021-02-12 22:08:30 +0800

    change version to 0.4.0

--- client/version.h
+++ client/version.h
@@ -7,13 +7,15 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               0
-#define VERSION_MINOR               3
-#define VERSION_REVISION            3
+#define VERSION_MINOR               4
+#define VERSION_REVISION            0
 
 #define XDAG_VERSION STRINGIZE(VERSION_MAJOR) "." STRINGIZE(VERSION_MINOR) "." STRINGIZE(VERSION_REVISION)
 
 
 /* version history
+
+0.4.0 change pow to randomx
 
 0.3.3 add height for mainblock
 
